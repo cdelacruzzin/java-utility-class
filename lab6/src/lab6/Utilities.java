1a5f23193822151eae702ac7ace3cb21de34d5d5
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2024-08-10 02:43:14 -0400

    Initial commit

---
+++ lab6/src/lab6/Utilities.java
@@ -0,0 +1,93 @@
+package lab6;
+
+/*
+ * Requirements:
+ * - Any use of Java library classes or methods (e.g., ArrayList) is forbidden.
+ * 	 (That is, there must not be an import statement in the beginning of this class.)
+ * Violation of this requirement will result in a 50% penalty of your marks.
+ *
+ * - Use only if-statements and/or loops to manipulate primitive arrays (e.g., int[], String[]).
+ */
+
+public class Utilities {
+    /*
+     * Input parameters:
+     * 	- `numbers` : an array of integers
+     *
+     * Refer to you lab instructions for what the method should return.
+     */
+    public static int[] getMultiplesOf3(int[] numbers) {
+
+        /* TODO: Your implementation ends here. */
+
+        /* Your implementation of this method starts here.
+         * 1. No System.out.println statements should appear here.
+         * 	  Instead, you need to return the result.
+         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+         *    Instead, refer to the input parameters of this method.
+         */
+        return null; // You need to replace this line by returning the proper result.
+    }
+
+    /*
+     * Input parameters:
+     * 	- `numbers` : an array of integers
+     *
+     * Assumptions:
+     * 	- numbers.length >= 1
+     *
+     * Refer to you lab instructions for what the method should return.
+     */
+    public static String[] getAllPrefixes(int[] numbers) {
+
+        /* TODO: Your implementation ends here. */
+
+        /* Your implementation of this method starts here.
+         * 1. No System.out.println statements should appear here.
+         * 	  Instead, you need to return the result.
+         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+         *    Instead, refer to the input parameters of this method.
+         */
+        return null; // You need to replace this line by returning the proper result.
+    }
+
+    /*
+     * Input parameters:
+     * 	- `numbers` : an array of integers
+     *
+     * Refer to you lab instructions for what the method should return.
+     */
+    public static int[] getGroupedNumbers(int[] numbers) {
+
+        /* TODO: Your implementation ends here. */
+
+        /* Your implementation of this method starts here.
+         * 1. No System.out.println statements should appear here.
+         * 	  Instead, you need to return the result.
+         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+         *    Instead, refer to the input parameters of this method.
+         */
+        return null; // You need to replace this line by returning the proper result.
+    }
+
+    /*
+     * Input parameters:
+     * 	- `twoDimArray` : a 2D array of integers
+     *
+     * Note: the sub-arrays may have different sizes.
+     *
+     * Refer to you lab instructions for what the method should return.
+     */
+    public static int[] zigZagFlatten(int[][] twoDimArray) {
+
+        /* TODO: Your implementation ends here. */
+
+        /* Your implementation of this method starts here.
+         * 1. No System.out.println statements should appear here.
+         * 	  Instead, you need to return the result.
+         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+         *    Instead, refer to the input parameters of this method.
+         */
+        return null; // You need to replace this line by returning the proper result.
+    }
+}
