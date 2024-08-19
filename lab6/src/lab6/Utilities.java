b7acde29370616e9efbe63a909a838c859b0fe06
CarlosDelaCruz18 <carlos18@my.yorku.ca>
2024-08-19 02:35:01 -0400

    finished most of test cases

--- lab6/src/lab6/Utilities.java
+++ lab6/src/lab6/Utilities.java
@@ -18,76 +18,119 @@ public class Utilities {
      */
     public static int[] getMultiplesOf3(int[] numbers) {
 
-        /* TODO: Your implementation ends here. */
-
-        /* Your implementation of this method starts here.
-         * 1. No System.out.println statements should appear here.
-         * 	  Instead, you need to return the result.
-         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
-         *    Instead, refer to the input parameters of this method.
-         */
-        return null; // You need to replace this line by returning the proper result.
+        int[] multiples = new int[0];
+        for (int number : numbers) {
+            for (int j = 3; j <= number; j *= 2) {
+                if (j == number) {
+                    multiples = addToArray(j, multiples);
+                }
+            }
+        }
+        return multiples; // You need to replace this line by returning the proper result.
     }
 
-    /*
-     * Input parameters:
-     * 	- `numbers` : an array of integers
-     *
-     * Assumptions:
-     * 	- numbers.length >= 1
-     *
-     * Refer to you lab instructions for what the method should return.
-     */
+
+
+
     public static String[] getAllPrefixes(int[] numbers) {
 
-        /* TODO: Your implementation ends here. */
+        String[] result = new String[numbers.length];
 
-        /* Your implementation of this method starts here.
-         * 1. No System.out.println statements should appear here.
-         * 	  Instead, you need to return the result.
-         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
-         *    Instead, refer to the input parameters of this method.
-         */
-        return null; // You need to replace this line by returning the proper result.
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = "[";
+            for (int j = 0; j <= i ; j++) {
+                if (j == 0) {
+                    result[i] += String.format("%d", numbers[j]);
+                } else {
+                    result[i] += String.format(", %d", numbers[j]);
+                }
+            }
+            result[i] += "]";
+        }
+        return result; // You need to replace this line by returning the proper result.
     }
 
-    /*
-     * Input parameters:
-     * 	- `numbers` : an array of integers
-     *
-     * Refer to you lab instructions for what the method should return.
-     */
+
     public static int[] getGroupedNumbers(int[] numbers) {
+        int[] result = new int[numbers.length];
+        StringBuilder gp1 = new StringBuilder();
+        StringBuilder gp2 = new StringBuilder();
+        StringBuilder gp3 = new StringBuilder();
+        for (int number : numbers) {
+            if (number % 3 == 0) {
+                gp1.append(number); // instead of string concatenation (+=), use .append()
+            } else if (number % 3 == 1) {
+                gp2.append(number);
+            } else if (number % 3 == 2) {
+                gp3.append(number);
+            }
+        }
+        String[] gp1Int = gp1.toString().split("");
+        String[] gp2Int = gp2.toString().split("");
+        String[] gp3Int = gp3.toString().split("");
 
-        /* TODO: Your implementation ends here. */
-
-        /* Your implementation of this method starts here.
-         * 1. No System.out.println statements should appear here.
-         * 	  Instead, you need to return the result.
-         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
-         *    Instead, refer to the input parameters of this method.
-         */
-        return null; // You need to replace this line by returning the proper result.
+        if (!gp1Int[0].isEmpty()) {
+            for (int i = 0; i < gp1Int.length; i++) {
+                result[i] = Integer.parseInt(gp1Int[Integer.parseInt(String.valueOf(i))]);
+            }
+        } else {
+            gp1Int = new String[0];
+        }
+        if (!gp2Int[0].isEmpty()) {
+            for (int i = 0; i < gp2Int.length; i++) {
+                result[gp1Int.length  + i] = Integer.parseInt(gp2Int[i]);
+            }
+        } else {
+            gp2Int = new String[0];
+        }
+        if (!gp3Int[0].isEmpty()) {
+            for (int i = 0; i < gp3Int.length; i++) {
+                result[gp1Int.length + gp2Int.length + i] = Integer.parseInt(gp3Int[i]);
+            }
+        } else {
+            gp3Int = new String[0];
+        }
+        return result; // You need to replace this line by returning the proper result.
     }
 
-    /*
-     * Input parameters:
-     * 	- `twoDimArray` : a 2D array of integers
-     *
-     * Note: the sub-arrays may have different sizes.
-     *
-     * Refer to you lab instructions for what the method should return.
-     */
+
     public static int[] zigZagFlatten(int[][] twoDimArray) {
 
-        /* TODO: Your implementation ends here. */
+        int[] zigzag = new int[0];
 
-        /* Your implementation of this method starts here.
-         * 1. No System.out.println statements should appear here.
-         * 	  Instead, you need to return the result.
-         * 2. No Scanner operations should appear here (e.g., input.nextInt()).
-         *    Instead, refer to the input parameters of this method.
-         */
-        return null; // You need to replace this line by returning the proper result.
+        for (int i = 0; i < twoDimArray.length; i++) {
+            if (twoDimArray[i] == null || twoDimArray[i].length == 0) {
+                continue;
+            }
+            if (i % 2 == 0) {
+                for (int j = 0; j < twoDimArray[i].length; j++) {
+                    int[] temp = new int[zigzag.length + 1];
+                    System.arraycopy(zigzag, 0, temp, 0, zigzag.length); //use instead of manual array copy
+                    temp[temp.length - 1] = twoDimArray[i][j];
+                    zigzag = temp;
+                }
+            } else {
+                for (int j = twoDimArray.length - 1; j >= 0; j--) {
+                    int[] temp = new int[zigzag.length + 1];
+                    for (int k = 0; k < zigzag.length; k++) {
+                        temp[k] = zigzag[k];
+                    }
+                    temp[temp.length - 1] = twoDimArray[i][j];
+                    zigzag = temp;
+                }
+            }
+
+        }
+
+        return zigzag; // You need to replace this line by returning the proper result.
+    }
+
+    public static int[] addToArray(int n, int[] array) {
+        int[] temp = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            temp[i] = array[i];
+        }
+        temp[array.length] = n;
+        return temp;
     }
 }
